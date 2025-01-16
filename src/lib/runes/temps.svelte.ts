bad1e1db12e5c7c9e5cf51e0a8d43cac8497c627
Bernd Malle <bernd.malle@gmail.com>
2025-01-16 20:41:00 +0700

    Svelte does NOT re-run the getter upon the backing fields value change, as long as it is not a $state - there must have been some cachine issue which made it appear that way (although I deleted the .svelte-kit folder)...
        implement 2-way binding in pure Runes with $state + $effect
    
        add TW4

--- src/lib/runes/temps.svelte.ts
+++ src/lib/runes/temps.svelte.ts
@@ -1,7 +1,36 @@
-// derived Runes version
-export class RuneTemps {
+export class StateGetSetTemps {
+	// NOTE these can be private or public, the reason #c is private here is solely so that we can mask it with the public getter/setter
+	// NOTE the $state is necessary for Svelte to know that the getter must be re-run on $state change,
+	// NOTE however, for some (caching?) reason, removal of $state might not immediately break the behavior...
+	#c = $state(0);
+	#f = $state(32);
+
+	get c() {
+		return this.#c;
+	}
+
+	get f() {
+		return this.#f;
+	}
+
+	set c(c: number) {
+		this.#c = c;
+		this.#f = c * (9 / 5) + 32;
+	}
+
+	set f(f: number) {
+		this.#f = f;
+		this.#c = ((f - 32) * 5) / 9;
+	}
+}
+
+export class DeriveEffectTemps {
 	c = $state(0);
 	f = $state(32);
 	f_derived = $derived((this.c * 9) / 5 + 32);
 	c_derived = $derived(((this.f - 32) * 5) / 9);
+	// NOTE for 2-way binding on Runes
+	// NOTE the $effects belonging to this must still run separately.
+	c_input: HTMLInputElement = null as unknown as HTMLInputElement;
+	f_input: HTMLInputElement = null as unknown as HTMLInputElement;
 }
--- src/routes/temps/+page.svelte
+++ src/routes/temps/+page.svelte
@@ -1,34 +1,61 @@
 <script lang="ts">
-	import { GetSetTemps } from '$lib/generic/temps';
-	import { RuneTemps } from '$lib/runes/temps.svelte';
+	import { StateGetSetTemps, DeriveEffectTemps } from '$lib/runes/temps.svelte';
 	import { nano_c, nano_f, nano_c_computed, nano_f_computed } from '$lib/nano/temps';
 
-	const getSetTemps = new GetSetTemps();
-	const runeTemps = new RuneTemps();
+	const getSetTemps = new StateGetSetTemps();
+	const runeTemps = new DeriveEffectTemps();
+
+	// NOTE for 2-way binding on Runes
+	$effect(() => {
+		const newFVal = (runeTemps.c * 9) / 5 + 32;
+		const newFLen = newFVal.toString().split('.')[1]?.length || 0;
+		runeTemps.f_input.value = newFLen > 4 ? newFVal.toFixed(4) : newFVal.toString();
+	});
+
+	// NOTE this effect must run separately from the above effect !!
+	$effect(() => {
+		const newC = ((runeTemps.f - 32) * 5) / 9;
+		const newCLen = newC.toString().split('.')[1]?.length || 0;
+		runeTemps.c_input.value = newCLen > 4 ? newC.toFixed(4) : newC.toString();
+	});
 </script>
 
 <h3>Getter/Setter - 2-way binding</h3>
 <input type="number" bind:value={getSetTemps.c} />
-<span>Celsius =</span>
+<span>C =</span>
 <input type="number" bind:value={getSetTemps.f} />
-<span>Fahrenheit</span>
+<span>F</span>
 <!-- <br/><span>pub_f_no_getter: {getSetTemps.pubf}</span>
 <br/><span>pub_f_no_$state: {getSetTemps.pubfnostate}</span> -->
 
-<h3>Svelte $derived - 1-way binding</h3>
+<h3>Runes $state + $effect - 2-way binding</h3>
+<input
+	type="number"
+	bind:this={runeTemps.c_input}
+	onkeyup={(e) => (runeTemps.c = +e.currentTarget.value)}
+/>
+<span>C =</span>
+<input
+	type="number"
+	bind:this={runeTemps.f_input}
+	onkeyup={(e) => (runeTemps.f = +e.currentTarget.value)}
+/>
+<span>F</span>
+
+<h3>Runes $derived - 1-way binding</h3>
 <input type="number" bind:value={runeTemps.c} />
-<span>Celsius =</span>
-<span>{runeTemps.f_derived} Fahrenheit</span>
+<span>C =</span>
+<span>{runeTemps.f_derived} F</span>
 <br />
 <input type="number" bind:value={runeTemps.f} />
-<span>Fahrenheit =</span>
-<span>{runeTemps.c_derived} Celsius</span>
+<span>F =</span>
+<span>{runeTemps.c_derived} C</span>
 
-<h3>NanoStores Derived</h3>
+<h3>NanoStores - computed - 1-way</h3>
 <input type="number" bind:value={$nano_c} />
-<span>Celsius =</span>
-<span>{$nano_f_computed} Fahrenheit</span>
+<span>C =</span>
+<span>{$nano_f_computed} F</span>
 <br />
 <input type="number" bind:value={$nano_f} />
-<span>Fahrenheit =</span>
-<span>{$nano_c_computed} Celsius</span>
+<span>F =</span>
+<span>{$nano_c_computed} C</span>
