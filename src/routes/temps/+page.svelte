9be07365712b679c4d6920c1768ab63d74a6a623
Bernd Malle <bernd.malle@gmail.com>
2025-01-16 21:11:33 +0700

    first attempt integrating xoid -> importing useAtom crashes Svelte5(kit)

--- src/routes/temps/+page.svelte
+++ src/routes/temps/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { StateGetSetTemps, DeriveEffectTemps } from '$lib/runes/temps.svelte';
 	import { nano_c, nano_f, nano_c_computed, nano_f_computed } from '$lib/nano/temps';
+	
+	// import { useAtom } from '@xoid/svelte';
+	// import { _xoid_c, _xoid_f } from '$lib/xoid/temps';
 
 	const getSetTemps = new StateGetSetTemps();
 	const runeTemps = new DeriveEffectTemps();
+	// let xoid_c = useAtom(_xoid_c);
+	// let xoid_f = useAtom(_xoid_f);
 
 	// NOTE for 2-way binding on Runes
 	$effect(() => {
@@ -20,7 +25,7 @@
 	});
 </script>
 
-<h3>Getter/Setter - 2-way binding</h3>
+<h3>Runes $state + get/set: 2-way binding</h3>
 <input type="number" bind:value={getSetTemps.c} />
 <span>C =</span>
 <input type="number" bind:value={getSetTemps.f} />
@@ -28,7 +33,7 @@
 <!-- <br/><span>pub_f_no_getter: {getSetTemps.pubf}</span>
 <br/><span>pub_f_no_$state: {getSetTemps.pubfnostate}</span> -->
 
-<h3>Runes $state + $effect - 2-way binding</h3>
+<h3>Runes $state + $effect: 2-way binding</h3>
 <input
 	type="number"
 	bind:this={runeTemps.c_input}
@@ -42,7 +47,7 @@
 />
 <span>F</span>
 
-<h3>Runes $derived - 1-way binding</h3>
+<h3>Runes $derived: 1-way binding</h3>
 <input type="number" bind:value={runeTemps.c} />
 <span>C =</span>
 <span>{runeTemps.f_derived} F</span>
@@ -51,7 +56,7 @@
 <span>F =</span>
 <span>{runeTemps.c_derived} C</span>
 
-<h3>NanoStores - computed - 1-way</h3>
+<h3>NanoStores computed: 1-way</h3>
 <input type="number" bind:value={$nano_c} />
 <span>C =</span>
 <span>{$nano_f_computed} F</span>
@@ -59,3 +64,9 @@
 <input type="number" bind:value={$nano_f} />
 <span>F =</span>
 <span>{$nano_c_computed} C</span>
+
+<!-- <h3>Xoid -> atom + "atom functions"</h3>
+<input type="number" bind:value={xoid_c} />
+<span>C =</span>
+<input type="number" bind:value={xoid_f} />
+<span>F</span> -->
