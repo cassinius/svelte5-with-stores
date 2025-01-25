0c8dc4a8869620984ebf52d10090536fc64ed081
Bernd Malle <bernd.malle@gmail.com>
2025-01-25 13:13:26 +0700

    add Daisyui & re-write config / styles

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -13,28 +13,16 @@
 <h3>Runes counter</h3>
 <p>
 	Count: {runesCounter.count}
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => runesCounter.count++}>+</button
-	>
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => runesCounter.count--}>-</button
-	>
+	<button class="btn btn-success btn-sm" onclick={() => runesCounter.count++}>+</button>
+	<button class="btn btn-error btn-sm" onclick={() => runesCounter.count--}>-</button>
 </p>
 <p>Double: {runesCounter.double}</p>
 
 <h3>Nano counter</h3>
 <p>
 	Count: {$nanoCount}
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => setCount($nanoCount + 1)}>+</button
-	>
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => setCount($nanoCount - 1)}>-</button
-	>
+	<button class="btn btn-success btn-sm" onclick={() => setCount($nanoCount + 1)}>+</button>
+	<button class="btn btn-error btn-sm" onclick={() => setCount($nanoCount - 1)}>-</button>
 </p>
 <p>Double: {$nanoDouble}</p>
 
@@ -42,13 +30,7 @@
 <h3>Alien counter</h3>
 <p>
 	Count: {$alienCount}
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => $alienCount++}>+</button
-	>
-	<button
-		class="w-6 py-1 rounded-md hover:cursor-pointer border-t-neutral-400 bg-slate-300"
-		onclick={() => $alienCount--}>-</button
-	>
+	<button class="btn btn-success btn-sm" onclick={() => $alienCount++}>+</button>
+	<button class="btn btn-error btn-sm" onclick={() => $alienCount--}>-</button>
 </p>
 <p>Double: {$alienDouble}</p>
