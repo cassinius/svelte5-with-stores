c68b1a15f446da1cd7ea878f771faba5b74f37cd
Bernd Malle <bernd.malle@gmail.com>
2025-01-26 14:30:58 +0700

    bit of re-formatting

--- src/routes/crud/+page.svelte
+++ src/routes/crud/+page.svelte
@@ -5,67 +5,60 @@
 
 	const crud = createSvelteCrud();
 
-	const textAreaSize = $derived(Math.min(Math.max(crud.filteredUsers.length + 1, 5), 7));
+	const textAreaSize = $derived(Math.min(Math.max(crud.filteredUsers.length + 1, 7), 10));
 </script>
 
 <div class="w-screen h-screen flex flex-col items-center justify-center">
 	<div class="crud-wrap p-6 bg-slate-600 rounded-xl">
-		<h3 class="text-red-400">CRUnesD</h3>
+		<div class="crud-col-wrap flex flex-row gap-4 justify-center items-start">
+			<div class="left-col flex flex-col">
+				<h3 class="text-white text-2xl">CRUnesD</h3>
+				<input
+					type="text"
+					class="w-42 mb-2"
+					bind:value={crud.searchVal}
+					placeholder="Filter prefix"
+				/>
+				<select
+					class="userlist w-42 p-1 text-sm bg-slate-100 text-slate-700 rounded"
+					bind:value={crud.selectedUser}
+					size={textAreaSize}
+				>
+					{#each crud.filteredUsers as user (`${user.name} ${user.surname}`)}
+						<option
+							class="user flex flex-row gap-2 pl-2 text-right hover:cursor-pointer hover:text-white hover:bg-blue-600"
+							class:active={crud.selectedUser === user}
+							onclick={() => (crud.selectedUser = user)}
+						>
+							{user.surname}, {user.name}
+						</option>
+					{/each}
+				</select>
+			</div>
 
-		<input type="text" class="w-42 mb-2" bind:value={crud.searchVal} placeholder="Filter prefix" />
+			<div class="right-col flex flex-col gap-1 items-start text-sm">
+				<div class="mt-4 text-lg text-white w-100% text-left italic">Crud User</div>
 
-		<div class="flex flex-row gap-2 items-start">
-			<select
-				class="userlist w-42 p-1 text-sm bg-slate-100 text-slate-700 rounded"
-				bind:value={crud.selectedUser}
-				size={textAreaSize}
-			>
-				{#each crud.filteredUsers as user (`${user.name} ${user.surname}`)}
-					<option
-						class="user flex flex-row gap-2 pl-2 text-right hover:cursor-pointer hover:text-white hover:bg-blue-600"
-						class:active={crud.selectedUser === user}
-						onclick={() => (crud.selectedUser = user)}
-					>
-						{user.surname}, {user.name}
-					</option>
-				{/each}
-			</select>
+				<input type="text" class="w-42" bind:value={crud.crudUser.name} placeholder="Name" />
 
-			<div class="flex flex-col gap-1 items-start text-sm">
-				<div class="text-lg text-white w-100% text-left">Crud User</div>
-				<div class="flex flex-row gap-2 items-center">
-					<!-- Name: -->
-					<input type="text" class="w-42" bind:value={crud.crudUser.name} placeholder="Name" />
-				</div>
+				<input type="text" class="w-42" bind:value={crud.crudUser.surname} placeholder="Surname" />
 
-				<div class="flex flex-row gap-2 items-center">
-					<!-- Surname: -->
-					<input
-						type="text"
-						class="w-42"
-						bind:value={crud.crudUser.surname}
-						placeholder="Surname"
-					/>
-				</div>
+				<button class="w-42 btn btn-success btn-sm" onclick={crud.createUser}>Create</button>
+				<button
+					class="w-42 btn btn-warning btn-sm"
+					disabled={!crud.selectedUser}
+					onclick={crud.updateUser}>Update</button
+				>
+				<button
+					class="w-42 btn btn-error btn-sm"
+					disabled={!crud.selectedUser}
+					onclick={crud.deleteUser}>Delete</button
+				>
 			</div>
 		</div>
 
-		<div class="mt-2">
-			<button class="w-28 btn btn-success btn-sm" onclick={crud.createUser}>Create</button>
-			<button
-				class="w-28 btn btn-warning btn-sm"
-				disabled={!crud.selectedUser}
-				onclick={crud.updateUser}>Update</button
-			>
-			<button
-				class="w-28 btn btn-error btn-sm"
-				disabled={!crud.selectedUser}
-				onclick={crud.deleteUser}>Delete</button
-			>
-		</div>
-
 		{#if crud.successMsg || crud.errMsg}
-			<div class="crud-msgs-wrap mt-2 h-10" transition:slide>
+			<div class="crud-msgs-wrap mt-2 h-10 bg-slate-600" transition:slide>
 				{#if crud.successMsg}
 					<div class="text-green-400" in:fly={{ y: -100, duration: 300 }} out:fade>
 						{crud.successMsg}
@@ -83,9 +76,6 @@
 </div>
 
 <style>
-	/* .crud-msgs-wrap {
-		transition: height 0.3s;
-	} */
 	.user.active {
 		background-color: #132e9b;
 		color: white;
